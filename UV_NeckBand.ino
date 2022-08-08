13861177baa4ad9c5105ec7176a677234f28a27f
Toru Takata <tettou771@gmail.com>
2022-08-08 17:44:24 +0900

    analogWriteでPWM、実機で動作

--- UV_NeckBand.ino
+++ UV_NeckBand.ino
@@ -2,62 +2,55 @@ const float TAU = 6.28;
 const int vol1pin = A0;
 const int vol2pin = A1;
 const int vol2swPin = 2;
-const int ledPin = 3;
+const int ledPin = 7;
 
 // for debug
 bool debug = true;
 uint32_t pastPrint = 0;
+
+float v1, v2;
+uint32_t past;
+float brightness;
 
 void setup() {
   pinMode(INPUT, vol1pin);
   pinMode(INPUT, vol2pin);
   pinMode(INPUT_PULLUP, vol2swPin);
   pinMode(OUTPUT, ledPin);
+  v1 = v2 = 0;
+  past = millis();
+  brightness = 0;
 }
 
 void loop() {
-  int v1 = analogRead(vol1pin);
-  int v2 = analogRead(vol2pin);
+  v1 += (analogRead(vol1pin) - v1) * 0.1;
+  v2 += (analogRead(vol2pin) - v2) * 0.1;
   bool v2sw = digitalRead(vol2swPin);
   uint32_t t = millis();
   
-  int brightnessMax = v1 / 4;
-  int brightness = 0;
-  bool blinkEnabled = v2sw == LOW;
+  float brightnessMax = v1 / 4;
+  bool blinkEnabled = (v2sw == LOW);
 
   if (blinkEnabled) {
-      float blinkFreq = v2 / 100. + 1.;
-      int brightness = brightnessMax * abs(sin(blinkFreq * TAU * t / 2000.));
+    int32_t blinkFreq = 2. + v2 / 100.; // Hz
+    int32_t deltaTime = t - past;
+    int32_t diff = 1. * brightnessMax * deltaTime * blinkFreq / 1000.;
+    brightness -= diff;
+    if (brightness < 0) brightness += brightnessMax;
   }
   // not blink
   else {
     brightness = brightnessMax;
   }
 
-  // manual PWM
-  float phase = 0;
-  float threshold = 0;
-  {
-    long freq = 1; // Hz
-    long term = 1000000 / freq; // usec
-    long current = micros() % term;
-    threshold = 1. * brightness / 255.;
-    phase = 1. * current / term;
-    if (phase < threshold) {
-      digitalWrite(ledPin, HIGH);
-    }else {
-      digitalWrite(ledPin, LOW);
-    }
-  }
+  analogWrite(ledPin, brightness);
 
   if (debug) {
     uint32_t now = millis();
     if (now - pastPrint >= 100) {
       pastPrint = now;
-      Serial.print(" phase ");
-      Serial.print(phase);
-      Serial.print(" th ");
-      Serial.print(threshold);
+      Serial.print(" b ");
+      Serial.print(brightness);
       Serial.print(" v1 ");
       Serial.print(v1);
       Serial.print(" v2 ");
@@ -67,4 +60,7 @@ void loop() {
       Serial.println();
     }
   }
+
+  past = t;
+  delay(10);
 }
